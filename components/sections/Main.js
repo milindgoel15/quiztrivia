8fdf819594e33232b2ac763a4be2459fe0a7c389
Milind Goel <milindgoel15@gmail.com>
2022-06-26 11:15:35 +0530

    fixed naming

--- components/sections/Main.js
+++ components/sections/Main.js
@@ -1,5 +1,5 @@
 import React, { useContext } from "react";
-import UserContext from "../../contexts/userContext";
+import UserContext from "../../contexts/UserContext";
 
 function Main({ getQuestions }) {
    let { setName } = useContext(UserContext);
